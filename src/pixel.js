ad46753559e982c3cb21b5fb6680070393bfa7eb
Jonathan Ferreira <jonathanferreiras@outlook.com>
2019-10-20 07:56:37 -0300

    Correção

---
+++ src/pixel.js
@@ -0,0 +1,84 @@
+
+    window.onload = function() {
+       
+        var cordinates = {}
+        var heatDiv = document.getElementsByTagName('html')[0];      
+        
+       
+        var script = document.createElement('script');
+        script.src = "https://unpkg.com/axios/dist/axios.min.js";
+        document.getElementsByTagName('head')[0].appendChild(script);
+        getData()
+        getClick()
+    
+    
+        window.onunload = function () {
+          
+            send()
+        }
+    
+        
+    
+        var links = document.querySelectorAll("a");
+        for (var i = 0; i < links.length; i++) {
+            links[i].addEventListener("click", function (e) {
+            //alert('O elemento clicado foi o ' + this.innerHTML);
+            send()    
+            })
+        }
+    
+        async function send() {
+            req = {
+                "user": idUser,
+                "url":  window.location.href,
+                "heatmap": JSON.stringify(cordinates)
+            }
+
+            axios({
+                method: 'post', // verbo http
+                url: 'https://heatmap404.herokuapp.com/api/heatmap/notify', // url
+                data: { req }
+                })
+                .then(response => {
+                    console.log(response)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+
+            cordinates = {}
+            getData()
+
+            }
+    
+            function getClick() {  
+                
+                 heatDiv.onclick = heatDiv.onclick = function(e) {
+                    e.preventDefault();
+                    var x = e.layerX;
+                    var y = e.layerY;
+                    // if (e.touches) {
+                    //   x = e.touches[0].pageX;
+                    //   y = e.touches[0].pageY;
+                    // }
+                  
+                   cordinates[date].push({x,y})
+                 
+            
+                }    
+           
+            
+            }
+
+            function getData() {
+                var today = new Date()
+                    year = today.getFullYear()
+                    day = today.getDay() < 10 ? '0' + today.getDay() : today.getDay()
+                    console.log(day)
+                    month = today.getMonth() < 10 ? '0' + today.getMonth() : today.getMonth()
+                    date = year + '-' + month + '-'+ day
+        
+                    cordinates[date] = []
+            }
+        }
+        
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors');
 const requireDirectory = require('require-dir');
 const path = require('path');
+process.env.PORT = 3030
 
 requireDirectory('./Entity');
 
@@ -19,6 +20,7 @@ mongoose.connect(
 app.use(express.json());
 app.use(cors());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'assets')));
+app.use('/pixel', express.static(path.resolve(__dirname, 'pixel.js')));
 
 app.use('/api', require('./routes'));
 
